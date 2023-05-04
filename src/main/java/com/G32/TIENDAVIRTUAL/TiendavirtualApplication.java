c9b392987d6ba32359bca2d5c5401afaa9744403
Sebastian Cepeda <scepeda192@gamil.com>
2023-05-03 21:37:05 -0500

    Primer commit

---
+++ src/main/java/com/G32/TIENDAVIRTUAL/TiendavirtualApplication.java
@@ -0,0 +1,13 @@
+package com.G32.TIENDAVIRTUAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendavirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/G32/TIENDAVIRTUAL/TiendavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G32.TIENDAVIRTUAL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
